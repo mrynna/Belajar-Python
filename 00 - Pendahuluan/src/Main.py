2abefa6dc857341669ddcd4d3f8abb71fb014e28
mrynna <muhrianardiansah@gmail.com>
2021-08-28 20:39:36 +0800

    Menambahkan 00 - Pendahuluan

---
+++ 00 - Pendahuluan/src/Main.py
@@ -0,0 +1,23 @@
+## Comment pada python menggunakan simbol #, 
+## Syntax pada python akan dijalankan baris per baris
+## Baris yang kosong tidak akan dijalankan
+## Barisnya harus sesuai dengan letaknya artinya selurus dengan perintah yang diberikan
+## Pada python tidak perlu menambahkan titik koma pada setiap barisnya
+
+print("Hello world") 
+print("Testing")
+
+"""Multiline comment digunakan jika kita ingin menambahkan comment dengan multi baris
+contoh nya ini
+kita bisa menggunakan beberapa baris
+caranya dengan menambahkan simbol " sebanyak tiga disetiap awal dan akhir comment"""
+
+## Kita bisa membuat variable pada python tanpa mendeklarasikan tipe datanya
+ 
+a = 10; 
+print(a)
+
+## Python merupakan interpreted language yang artinya file python tidak perlu dicompile untuk dapat dijalankan
+## Tetapi kita bisa mengcompile file python menjadi sebuah file bytecode
+## Compile pada python berfungsi agar jalannya program python menjadi lebih efisien
+## Cara untuk mengcompile adalah, dengan menggunakan "python -m py_compile namafile.py" tanpa tanda kutip 
