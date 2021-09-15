723ca17d13481a5e7a9ed9cc5f1cc4e39bfb1856
mrynna <muhrianardiansah@gmail.com>
2021-09-15 23:05:26 +0800

    Menambahkan 22 - Break, Continue and Else Clauses

---
+++ 22 - Break, Continue and Else Clauses on Loop (Flow Control)/README.md
@@ -0,0 +1,5 @@
+# 22 - Break, Continue and Else Clauses on loop
+
+## Folder Structure
+
+- `src`: Folder yang menyimpan file source code
---
+++ 22 - Break, Continue and Else Clauses on Loop (Flow Control)/src/Main.py
@@ -0,0 +1,28 @@
+# Break menghentikan jalannya program
+print("="*50)
+print("Contoh Break")
+kondisi = True
+while kondisi:
+    kondisi = input("masukkan n untuk menghentikan perulangan : ")
+    if kondisi == 'n':
+        break # Jika kondisi bernilai n, maka akan dijalankan break, yang akan mengehntikan perulangan
+print("Perulangan berakhir")
+
+# Continue, melangkahi, untuk melanjutkan jalannya program tanpa menjalankan perintah setelah continue, jadi statement akan dilangkahi namun perulangan akan tetap berlanjut
+print("="*50)
+print("Contoh Continue")
+for i in range(20):
+    if i%2 == 0: # Jika i modulus 2 = 0 maka continue, yang artinya nilai yang ketika dibagi 2 menghasilkan nilai 0, maka akan dilangkahi
+        continue
+    print("Nilai i = ", i) 
+
+# Else Clauses
+print("="*50)
+print("Contoh Else Clauses")
+for i in range(20):
+    for n in range(0, i):
+        if i%2 == 0:
+            print(f"{i} adalah bilangan genap")
+            break
+    else:
+        print(f"{i} adalah bilangan ganjil")
